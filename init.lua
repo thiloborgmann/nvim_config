bc73d5feadb507993b8daeb3b685effa85d10066
Thilo Borgmann <thilo.borgmann@mail.de>
2023-03-03 00:57:38 +0100

    Add shortcut for git status to Telescope

--- init.lua
+++ init.lua
@@ -285,6 +285,9 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
+-- Add som  keybindings for Git, Telescope
+vim.keymap.set({ 'n', 'v' }, '<Leader>gs', ':G<CR>', { silent = true, desc = '[G]it [S]tatus' })
+
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
